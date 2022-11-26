6e12e105c9f986b462b6b2c703a67f350554f547
H3153N <87572254+H3153N@users.noreply.github.com>
2022-11-26 14:50:47 -0300

    Add files via upload

---
+++ src/Biblioteca.cpp
@@ -0,0 +1,24 @@
+#include "Biblioteca.h"
+
+vector<livro> Biblioteca::get_livros_autor(std::string autor)
+{
+
+}
+vector<Livro> Biblioteca::get_livros_genero(std::string genero)
+{
+
+}
+vector<Livro> Biblioteca::get_livros_nome(std::string nome)
+{
+
+}
+
+
+void Biblioteca::update_estoque(Livro livro)
+{
+
+}
+void Biblioteca::update_pessoas(Pessoa pessoa)
+{
+
+}
