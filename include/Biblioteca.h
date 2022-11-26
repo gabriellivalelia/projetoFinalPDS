b90bc45fbbcfebc7435965374f71e8f75a797f49
H3153N <87572254+H3153N@users.noreply.github.com>
2022-11-26 14:49:56 -0300

    Add files via upload

---
+++ include/Biblioteca.h
@@ -0,0 +1,18 @@
+#ifndef TRAB_PDS2_BIBLIOTECA_H
+#define TRAB_PDS2_BIBLIOTECA_H
+
+
+
+class Biblioteca
+{
+    private:
+        vector<Livro>_livros_estoque;
+        vector<Pessoa>_pessoas;
+    public:
+        vector<livro> get_livros_nome(std::string nome);
+        vector<livro> get_livros_autor(std::string autor);
+        vector<livro> get_livros_genero(std::string genero);
+
+        void update_estoque(Livro livro);
+        void update_pessoas(Pessoa pessoa);
+};
