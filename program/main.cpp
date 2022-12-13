88e73058832766bdee23fb40e4d6468fd0fea041
H3153N <87572254+H3153N@users.noreply.github.com>
2022-12-13 16:29:47 -0300

    Update main.cpp

--- program/main.cpp
+++ program/main.cpp
@@ -3,14 +3,21 @@
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-
+    //std::cout << "Hello World!" << std::endl;
+    //se transformar a biblioteca em uma classe estática, não vai precisar de criar instância
     Biblioteca BibliotecaTeste = Biblioteca();
-
     BibliotecaTeste.preencher_livros();
+    
 
     BibliotecaTeste.get_livros_autor("Júlia Queen");
 
+    while(!BibliotecaTeste.login())
+    {
+        system("cls");
+        std::cout << "Nome nao encontrado. Tente novamente." << std::endl;
+        //codigo vai ficar enrolando aqui enquanto o login não for realizado
+    }
 
+    //continuar embaixo
     return 0;
 }
