c37112fa33c81f58b0e1e684bdf69e59e4c93eea
GabrielliSilva <gabriellisilva@cpejr.com.br>
2022-12-20 11:40:06 -0300

    feat: doxygen pessoa.h

--- include/Pessoa.h
+++ include/Pessoa.h
@@ -4,29 +4,64 @@
 #include "Livro.h"
 #include <string>
 
-
+/** 
+* @brief Classe Pessoa que contém os atributos e métodos base dos dois tipos de usuário de nosso sistema. 
+*/ 
 class Pessoa {
     public:
-    private:  
+    private:
+        /**
+         * Nome da pessoa.
+         */   
         std::string _nome;
+        /**
+         * Senha da pessoa.
+         */   
         std::string _senha;
+        /**
+         * Booleano que recebe true caso a pessoa seja do tipo "Bibliotecário" e false caso seja do tipo "Usuário".
+         */   
         bool _isBibliotecario;
 
     public:
     
-     Pessoa(
+
+    /**
+     * Cria uma nova pessoa.
+     * @brief Constructor.
+     * @param nome O nome da pessoas.
+     * @param senha A senha da pessoa.
+     */
+    Pessoa(
         std::string nome,
         std::string senha
     );
-    
+    /**
+     * Cria uma nova pessoa.
+     * @brief Constructor.
+     * @param nome O nome da pessoas.
+     * @param senha A senha da pessoa.
+     * @param isBibliotecario True caso seja um Bibliotecário e false caso seja um usuário.
+     */
     Pessoa(
         std::string nome,
         std::string senha,
         bool isBibliotecario
     );
-    std::string get_nome();    
+    /**
+     * @brief Obtém o nome da pessoa corrente.
+     * @return Nome da pessoa corrente.
+     */
+    std::string get_nome();
+    /**
+     * @brief Obtém a senha da pessoa corrente.
+     * @return Senha da pessoa corrente.
+     */    
     std::string get_senha();
-
+    /**
+     * @brief Obtém o tipo da pessoa corrente.
+     * @return True caso seja um Bibliotecário e false caso seja um usuário.
+     */
     virtual bool get_tipo();
     
 };
