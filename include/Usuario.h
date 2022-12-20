4478654d37fc3eeea10031d44d4dc1c842c6ded3
GabrielliSilva <gabriellisilva@cpejr.com.br>
2022-12-20 11:52:55 -0300

    feat: doxygen usuario.h

--- include/Usuario.h
+++ include/Usuario.h
@@ -7,18 +7,40 @@
 #include <string>
 #include <iostream>
 
+/** 
+* @brief Classe Usuário que abstrai o usuário que pode alugar livros no sistema, devolvê-los e afins. Herda de Pessoa. 
+*/ 
 class Usuario : public Pessoa {
     private:
+        /**
+         * Vetor do tipo Livro que armazena os Livros que o usuário corrente alugou.
+         */  
         std::vector<Livro> _livros_alugados;
     public:
+        /**
+         * Cria um novo usuário.
+         * @brief Constructor.
+         * @param nome O nome do usuário.
+         * @param senha A senha do usuário.
+         */
         Usuario(
             std::string nome,
             std::string senha
         );
+        /**
+         * @brief Imprime todos os livros alugados pelo usuário corrente.
+         */
         void visualizar_livros_alugados();
-
+        /**
+         * @brief Adiciona um livro no vetor de livros alugados pelo usuário corrente.
+         * @param livro O livro que será adicionado.
+         * @return True caso a ação seja bem sucedida, o que acontece caso esse livro ainda não exista no vetor, e false caso contrário.
+         */
         bool adicionar_livro_no_vetor(Livro livro);
-
+        /**
+         * @brief Remove um livro do vetor de livros alugados pelo usuário corrente.
+         * @param livro O livro que será removido.
+         */
         void excluir_livro_do_vetor(Livro livro);
 
 
